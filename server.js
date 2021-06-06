aac779db02b8809e6a5314064e53c4874212d1b8
Nguyen Ong <giaanguyen221197@gmail.com>
2021-06-07 00:21:47 +0700

    no message

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
-
-app.use(express.static(__dirname + '/dist/articles'));
-
-app.get('/*', (req, res, next) => {
-  res.sendFile(path.join(__dirname + '/dist/articles/index.html'));
+app.use(express.static(__dirname + '/dist'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-
-app.listen(process.env.PORT || 8000);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
