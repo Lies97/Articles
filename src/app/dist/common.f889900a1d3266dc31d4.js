b7ad049f519c373c0055694b634a6f663fbd1c03
Nguyen Ong <giaanguyen221197@gmail.com>
2021-06-06 23:12:02 +0700

    test

---
+++ src/app/dist/common.f889900a1d3266dc31d4.js
@@ -0,0 +1,14 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [1],
+  {
+    '4u49': function (n, u, o) {
+      'use strict';
+      function t(n) {
+        return n;
+      }
+      o.d(u, 'a', function () {
+        return t;
+      });
+    },
+  },
+]);
